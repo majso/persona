6b1491cdb4e9fd51098b9020fa2324033fc2a4d7
Michal Grich <michal.grich@gmail.com>
2021-10-02 18:03:52 +0200

    0.2

---
+++ src/Menu.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Folder from './Folder.svelte';
+	import Modal from './Modal.svelte';
+
+
+	let files =[
+		{
+		type: 'file', 
+		name: "Jozko" 
+	},
+	{
+		type: 'file', 
+		name: "Dezko" 
+	}
+	]
+
+	const addFile = (e) => {
+
+	console.log(e);
+	}
+
+</script>
+
+<div class="column is-3">
+	<aside class="menu is-hidden-mobile aside">
+		<p class="menu-label">
+			Dashboard
+		</p>
+		<p class="menu-label">
+			Feeds
+		</p>
+		<Modal modalContent='<AddFile on:addfile={addFile}'/>
+
+		<Folder name="Subscriptions" files={files} expanded />
+		<p class="menu-label">
+			Settings
+		</p>
+	</aside>
+</div>
