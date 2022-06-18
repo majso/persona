33c2d88b89c6fbb2b722f7688b40002218ffa9b3
majso <michal.grich@gmail.com>
2022-06-19 00:55:05 +0200

    init

---
+++ api/db/database.ts
@@ -0,0 +1,11 @@
+import { DB } from '../../deps.ts';
+
+const db = new DB('database.sqlite');
+
+db.query(`
+  CREATE TABLE IF NOT EXISTS feeds (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    url TEXT
+  )
+`);
