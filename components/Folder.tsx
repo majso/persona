3c6e8fc65e5b33c5ecfd7ca13f322c53dd99998c
majso <michal.grich@gmail.com>
2022-07-04 23:59:20 +0200

    update

--- components/Folder.tsx
+++ components/Folder.tsx
@@ -18,11 +18,11 @@ export default function Folder() {
     },
   ];
   return (
-    <ul class={tw`relative`}>
+    <ul class={tw`flex flex-wrap flex-col `}>
       <div class={tw`flex items-center text-sm py-2 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap`}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" /></svg>        
+        <svg xmlns="http://www.w3.org/2000/svg"  className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="orange" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" /></svg>        
         <span>FEEDS</span>
-        <a class={tw`ml-auto hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer rounded`} href="/folder" title="Add Folder">
+        <a class={tw`ml-auto hover:bg-blue-50  transition duration-300 ease-in-out cursor-pointer rounded`} href="/folder" title="Add Folder">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" /></svg>
         </a>
       </div>
